a6db1d7dce9042e1e7e09524f6983148ab557c16
sachin-coder-hub <sachincoder0706@gmail.com>
2025-05-04 20:54:11 +0530

    NewsLetter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import MainBanner from '../components/MainBanner'
-import Categories from '../components/Categories'
-import Bestseller from '../components/Bestseller'
-import BottomBanner from '../components/BottomBanner'
+import React from "react";
+import MainBanner from "../components/MainBanner";
+import Categories from "../components/Categories";
+import Bestseller from "../components/Bestseller";
+import BottomBanner from "../components/BottomBanner";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
-    <div className='mt-10'>
-        <MainBanner/>
-        <Categories/>
-        <Bestseller/>
-        <BottomBanner/>
+    <div className="mt-10">
+      <MainBanner />
+      <Categories />
+      <Bestseller />
+      <BottomBanner />
+      <NewsLetter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
